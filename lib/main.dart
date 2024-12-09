1590afec93014607605abeb11ee081e6951c7782
pixelgame239 <117742483+pixelgame239@users.noreply.github.com>
2024-12-09 18:43:28 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://vcuvreljcgozbmtwvmyk.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjdXZyZWxqY2dvemJtdHd2bXlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3NzI0MzYsImV4cCI6MjA0NzM0ODQzNn0.xzIuopozExTOu-ZrY43DhlSTz3rsOCmSm8JrtKa8TjI',
+    url: 'SUPABASE-URL',
+    anonKey: 'SUPABASE-ANON-KEY',
   );
   runApp(ChangeNotifierProvider(create: (context)=>ThemeModel(),
   child: const MainApp(),) 
